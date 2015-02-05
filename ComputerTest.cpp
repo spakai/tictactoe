0642c8edc781c22dfdf5498890361b110e7b7083
Prakash S <spakai@gmail.com>
2015-02-05 20:29:56 +0800

    AI test

--- ComputerTest.cpp
+++ ComputerTest.cpp
@@ -1,8 +1,22 @@
 #include "gmock/gmock.h"
 #include "Computer.h"
-
+#include "Human.h"
+#include "Board.h"
+ 
 using namespace testing;
 using namespace std;
 
-TEST(ComputerAI, ComputerWins) {
+class AI: public Test {
+    public:
+        Board board;
+		Human human;
+		Computer ai;
+};
+
+TEST_F(AI, ComputerMakeMoveToWin) {
+	human.setCallSign(PlayerOptions::X);
+	ai.setCallSign(PlayerOptions::O);
+	board.makeMove(ai,0);
+	board.makeMove(ai,1);
+	ASSERT_THAT(ai.calculateMove(board), Eq(2));
 }
