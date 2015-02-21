4f06f62dd55ea03082a48ecb7124c882bc8a7c9a
Prakash S <spakai@gmail.com>
2015-02-21 12:48:51 +0800

    Fixed indentation

--- GameTest.cpp
+++ GameTest.cpp
@@ -8,18 +8,18 @@ using namespace std;
 TEST(AGame, GetWhoPlaysFirst) {
     Human PlayerOne;
     Human PlayerTwo;
-	Game game(PlayerOne, PlayerTwo);
-	GenericPlayer &player = game.whoPlaysFirst();
-	GenericPlayer &playerOne = game.getPlayerOne();
-	ASSERT_THAT(player.getCallSign(),playerOne.getCallSign());
+    Game game(PlayerOne, PlayerTwo);
+    GenericPlayer &player = game.whoPlaysFirst();
+    GenericPlayer &playerOne = game.getPlayerOne();
+    ASSERT_THAT(player.getCallSign(),playerOne.getCallSign());
 }
 
 TEST(AGame, Opponent) {
     Human PlayerOne;
     Human PlayerTwo;
-	Game game(PlayerOne, PlayerTwo);
-	GenericPlayer &playerOne = game.getPlayerOne();
-	GenericPlayer &opponent = game.opponent(playerOne);
-	EXPECT_THAT(playerOne.getCallSign(), PlayerOptions::X);
-	ASSERT_THAT(opponent.getCallSign(),PlayerOptions::O);
+    Game game(PlayerOne, PlayerTwo);
+    GenericPlayer &playerOne = game.getPlayerOne();
+    GenericPlayer &opponent = game.opponent(playerOne);
+    EXPECT_THAT(playerOne.getCallSign(), PlayerOptions::X);
+    ASSERT_THAT(opponent.getCallSign(),PlayerOptions::O);
 } 
